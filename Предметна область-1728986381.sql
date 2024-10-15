216e3547376d52826764f61dc4804e22d0458781
Iryna Panchenko <iryna.panchenko@lnu.edu.ua>
2024-10-15 13:22:58 +0300

    Предметна область-1728986381.sql

--- Предметна область-1728986381.sql
+++ Предметна область-1728986381.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `Customer` (
+CREATE TABLE IF NOT EXISTS `Customer` (
     `customer_id` INT AUTO_INCREMENT NOT NULL,
     `first_name` VARCHAR(100) NOT NULL,
     `last_name` VARCHAR(100) NOT NULL,
@@ -7,14 +7,14 @@ CREATE TABLE `Customer` (
     PRIMARY KEY (`customer_id`)
 );
 
-CREATE TABLE `Service` (
+CREATE TABLE IF NOT EXISTS `Service` (
     `service_id` INT AUTO_INCREMENT NOT NULL,
     `service_name` VARCHAR(100) NOT NULL,
     `price` DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (`service_id`)
 );
 
-CREATE TABLE `Reservation` (
+CREATE TABLE IF NOT EXISTS `Reservation` (
     `reservation_id` INT AUTO_INCREMENT NOT NULL,
     `check_in_date` DATE NOT NULL,
     `check_out_date` DATE NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE `Reservation` (
     PRIMARY KEY (`reservation_id`)
 );
 
-CREATE TABLE `Room` (
+CREATE TABLE IF NOT EXISTS `Room` (
     `room_id` INT AUTO_INCREMENT NOT NULL,
     `room_number` VARCHAR(20) NOT NULL,
     `room_type` VARCHAR(50) NOT NULL,
@@ -31,42 +31,9 @@ CREATE TABLE `Room` (
     PRIMARY KEY (`room_id`)
 );
 
-CREATE TABLE `Payment` (
+CREATE TABLE IF NOT EXISTS `Payment` (
     `payment_id` INT AUTO_INCREMENT NOT NULL,
     `payment_date` DATE NOT NULL,
     `amount` DECIMAL(10, 2) NOT NULL,
-    `reservation_id` INT NOT NULL,
-    `payment_method` VARCHAR(50) NOT NULL,
-    `customer_ID` INT NOT NULL,
     PRIMARY KEY (`payment_id`)
 );
-
-CREATE TABLE `Employee` (
-    `employee_id` INT AUTO_INCREMENT NOT NULL,
-    `name` VARCHAR(100) NOT NULL,
-    `position` VARCHAR(50) NOT NULL,
-    `work_schedule` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`employee_id`)
-);
-
-CREATE TABLE `Order` (
-    `order_id` INT AUTO_INCREMENT NOT NULL,
-    `reservation_id` INT NOT NULL,
-    `service_id` INT NOT NULL,
-    `quantity` INT NOT NULL,
-    `comments` TEXT,
-    PRIMARY KEY (`order_id`)
-);
-
--- Зв'язки між таблицями
-ALTER TABLE `Payment` 
-ADD CONSTRAINT `fk_payment_customer` FOREIGN KEY (`customer_ID`) REFERENCES `Customer`(`customer_id`),
-ADD CONSTRAINT `fk_payment_reservation` FOREIGN KEY (`reservation_id`) REFERENCES `Reservation`(`reservation_id`);
-
-ALTER TABLE `Reservation` 
-ADD CONSTRAINT `fk_reservation_room` FOREIGN KEY (`room_id`) REFERENCES `Room`(`room_id`),
-ADD CONSTRAINT `fk_reservation_employee` FOREIGN KEY (`employee_id`) REFERENCES `Employee`(`employee_id`);
-
-ALTER TABLE `Order` 
-ADD CONSTRAINT `fk_order_reservation` FOREIGN KEY (`reservation_id`) REFERENCES `Reservation`(`reservation_id`),
-ADD CONSTRAINT `fk_order_service` FOREIGN KEY (`service_id`) REFERENCES `Service`(`service_id`);
